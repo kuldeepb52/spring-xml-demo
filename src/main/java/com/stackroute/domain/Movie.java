63f863a055e446817210ae193634d2d73137d76f
kuldeep <kuldeepb52@gmail.com>
2019-09-26 11:46:31 +0530

    fourth

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,6 +1,13 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
     private Actor actorBean;
 
     public Movie(Actor actorBean) {
@@ -15,4 +22,21 @@ public class Movie {
     }
 
 
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("bean factory set");
+        System.out.println(beanFactory);
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("bean name set");
+        System.out.println(s);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("application context set");
+        System.out.println(applicationContext);
+    }
 }
